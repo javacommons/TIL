6c65bc15daa504f0a3b16b21683fdec5faba4101
javacommons <javacommons@gmail.com>
2019-03-14 00:03:54 +0900

    Update janino02.java

--- janino/janino02.java
+++ janino/janino02.java
@@ -2,8 +2,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.List;
 
 import org.codehaus.janino.Java;
+import org.codehaus.janino.Java.PackageMemberTypeDeclaration;
 import org.codehaus.janino.Parser;
 import org.codehaus.janino.Scanner;
 import org.codehaus.janino.UnparseVisitor;
@@ -20,9 +22,26 @@ public class Main {
 		Java.CompilationUnit cu = new Parser(new Scanner("test.java", fileReader)).parseCompilationUnit();
 		// Manipulate the AST in memory.
 		// ...
+		System.out.println(cu.getPackageMemberTypeDeclarations().length);
+		PackageMemberTypeDeclaration[] x = cu.getPackageMemberTypeDeclarations();
+		System.out.println(x[0].getClassName());
 		// Convert the AST back into text.
 		UnparseVisitor.unparse(cu, new OutputStreamWriter(System.out));
 		System.out.println("HELLO\u3042\u3044\u3046");
+		System.out.println(cu.optionalPackageDeclaration.packageName);
+		List<Java.CompilationUnit.ImportDeclaration> ids = cu.importDeclarations;
+		System.out.println(ids.size());
+		for(Java.CompilationUnit.ImportDeclaration id : ids) {
+			System.out.println(id.toString());
+		}
+		List<Java.PackageMemberTypeDeclaration> tds = cu.packageMemberTypeDeclarations;
+		for(Java.PackageMemberTypeDeclaration td: tds) {
+			System.out.println(td.toString());
+			System.out.println(td.getClassName());
+		}
+		tds.add(tds.get(0));
+		System.out.println(tds.size());
+		UnparseVisitor.unparse(cu, new OutputStreamWriter(System.out));
 	}
 
 }
