7dc433ed189cb50f87ff189aa597628c8a60ea4e
javacommons <javacommons@gmail.com>
2019-03-14 21:23:19 +0900

    Update B.java

--- testdata/B.java
+++ testdata/B.java
@@ -4,9 +4,12 @@ package pkg1.pkg2;
 
 import java.lang.*; import a.b.C;
 
-    class ABC {public String fldA="あいう"; List<String> fldB; }
+public @interface ExampleAnnotation {
+}
+
+class ABC {public String fldA="あいう"; List<String> fldB; }
     
-    public class B implements Runnable {
+public class B implements Runnable {
     public void run() {
         for(int i=0;i<10;i++) System.out.println("HELLOあいう");
     }
