dadb18fbc70516020f35365068911af5e2a1162c
javacommons <javacommons@gmail.com>
2019-03-14 21:25:21 +0900

    Update B.java

--- testdata/B.java
+++ testdata/B.java
@@ -7,9 +7,14 @@ import java.lang.*; import a.b.C;
 public @interface ExampleAnnotation {
 }
 
-class ABC {public String fldA="あいう"; List<String> fldB; }
+@LocationExampleAnnotation
+class ABC {
+    @LocationExampleAnnotation
+    public String fldA="あいう";
+    List<String> fldB; }
     
 public class B implements Runnable {
+    @LocationExampleAnnotation
     public void run() {
         for(int i=0;i<10;i++) System.out.println("HELLOあいう");
     }
